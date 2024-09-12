61e306e1eefe56af88d1bf8c16fab808e89deb1c
JMSBPP <jmsbpp@hotmail.es>
2024-09-12 14:23:57 -0500

    starting simulation

---
+++ src/contracts/script/deployments.s.sol
@@ -0,0 +1,11 @@
+import {Script} from "../lib/forge-std/src/Script.sol";
+import "../comet/contracts/CometConfiguration.sol";
+import "../comet/contracts/CometCore.sol";
+import "../comet/contracts/CometExt.sol";
+import "../comet/contracts/CometMath.sol";
+import "../comet/contracts/CometRewards.sol";
+import "../comet/contracts/CometStorage.sol";
+import "../comet/contracts/Comet.sol";
+import "../comet/contracts/CometMainInterface.sol";
+
+contract deployments is Script {}
